57de8a681c88be7c7201f2261cd8f4c90cbfc509
KumarG23 <sharma.neal@yahoo.com>
2024-04-18 14:54:39 -0400

    pretty much done

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A repo template built with Vite.
 ## Initial Setup for GitHub Pages deployment
 * in vite.config.js change 'base' value to the name of your repository
 * in GitHub: repo: settings: pages: branch: change from /root to /docs
+* before pushing do npm run build
 
 ## To Run Locally
 * $ nvm use 20
--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const square = document.querySelectorAll(".square"); // query all squares
 const statText = document.querySelector("#statText");
 const restartBtn = document.querySelector("#restartBtn");
-const winConditions = [
+let coinSound = document.querySelector("#coinSound");
+let winSound = document.querySelector('#winSound');
+let catSound = document.querySelector('#catSound');
+
+let winConditions = [
     // array defining win conditions
     [0,1,2],
     [3,4,5],
@@ -16,23 +20,50 @@ const winConditions = [
 let options = ["", "", "", "", "", "", "", "", ""];
 // keep track of current player "X" or "O"
 let currentPlayer = "X";
+
 // keep track if game is running or not
 let gameRunning = false;
 
-initializeGame();
 
-function initializeGame(){
-    square.forEach(square => square.addEventListener("click", squareClicked))// add event listener to each square
+
+//initializeGame();
+window.onload = function(){
+ let storedData = localStorage.getItem('board');
+if (storedData) {
+    options = JSON.parse(storedData);
+}
+let storedPlayer = localStorage.getItem('player');
+if (storedData) {
+    currentPlayer = JSON.parse(storedPlayer);
+}
+
+square.forEach((square, index) => { // add text content back in
+    // after local storage saved
+    square.textContent = options[index];
+    if (options[index] !== '') { // only if index does not = ''
+        square.classList.add(options[index]);
+    }
+})
+//console.log(options);
+
+
+  
+       square.forEach(square => square.addEventListener("click", squareClicked))// add event listener to each square
 
     restartBtn.addEventListener("click", restartGame);// add event listener to restart button
 
     statText.textContent = `${currentPlayer}'s turn`;// display current players turn
 
     gameRunning = true;// change game running to true
+    
+
+    
 }
 
 function squareClicked(){// function for when square is clicked
     let squareIndex = this.getAttribute("squareIndex");// get index of clicked square
+    coinSound.play();
+
 
     if(options[squareIndex] != "" || !gameRunning){// check if square is already occupied or the game isn't running
         return; // return nothing
@@ -46,11 +77,13 @@ function updateSquare(square, index){ // function to update state of game board
     options[index] = currentPlayer; // update options array with current players symbol
     square.textContent = currentPlayer; // display current players symbol in the square
     square.classList.add(currentPlayer);
+    localStorage.setItem('board', JSON.stringify(options))
+    //console.log(options);
+    localStorage.setItem('player', JSON.stringify(currentPlayer));
 }
 
 function changePlayer(){ // function to change player turn
     currentPlayer = (currentPlayer === "X") ? "O" : "X"; // change current player to other player
-
     statText.textContent = `${currentPlayer}'s turn`; // display current players turn
 }
 
@@ -73,11 +106,14 @@ function checkWin(){ // function to check for win or draw
     }
 
     if(winner){
-        statText.textContent = `${currentPlayer} Wins!`; // display winner 
+        statText.textContent = `${currentPlayer} Wins!🏆`;
+        winSound.play();
+         // display winner 
         gameRunning = false; // set game running to false
     }
     else if (!options.includes("")){ // if options does not include any spaces then its a draw
-        statText.textContent = "Cat! (Draw)"; // display text for draw
+        statText.textContent = "Cat! 😼 (Draw)"; // display text for draw
+        catSound.play();
         gameRunning = false; // stop game
     }
     else { // if no winner and there are still spaces change player
@@ -90,7 +126,7 @@ function restartGame(){ // function to restart game
     options = ["", "", "", "", "", "", "", "", ""]; // set options array back to empty
     statText.textContent = `${currentPlayer}'s turn`; // display current players turn
     square.forEach(square => square.textContent = ""); // set each square back to empty
-    gameRunning = true; // turn game back to trueeee
+    gameRunning = true; // turn game back to true
     
 }
 
